59c9f6dff6e886e818e2b4ee95c9591ea51adb92
Ellie <112726692+elliezub@users.noreply.github.com>
2023-05-09 19:05:08 -0400

    add

---
+++ script.js
@@ -0,0 +1,15 @@
+const content = document.getElementsByClassName("content")
+const tabButton = document.getElementsByClassName("tab-button")
+
+
+
+function openTab(evt, tabName) {
+for (let i = 0; i < content.length; i++) {
+    content[i].style.display = "none";
+}
+for (let i = 0; i < tabButton.length; i++) {
+    tabButton[i].className = tabButton[i].className.replace(" active", "");
+}
+ document.getElementById(tabName).style.display = "block";
+ evt.currentTarget.className += " active";
+}
